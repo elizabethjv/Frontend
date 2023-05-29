cb62d6ecfed968e8173bb7722813a6b7350ea229
Vikrant Katoch <vikrantkatoch2000@gmail.com>
2023-05-29 16:38:53 -0700

    Sched 48 (#4)
    * Added loader
    
    * updated color for loader

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import Main from './pages/Main'
+import Preloader from './components/Loading/PreLoader'
 
 function App() {
-    return (
-        <div>
-            <Main />
-        </div>
-    )
+    const [loading, setLoading] = useState(false)
+    useEffect(() => {
+        setLoading(true)
+        setTimeout(() => {
+            setLoading(false)
+        }, 4000)
+    }, [])
+    return <div>{loading ? <Preloader /> : <Main />}</div>
 }
 
 export default App
---
+++ src/components/Loading/PreLoader.tsx
@@ -0,0 +1,16 @@
+import './Loading.css'
+
+const PreLoader = () => {
+    return (
+        <div className='lo'>
+            <div className='container'>
+                <div className='load'>
+                    <div className='ball'></div>
+                    <div className='text'>LOADING</div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default PreLoader
